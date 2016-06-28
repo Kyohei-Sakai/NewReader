c788f3463667df99ab3d3a4db8976627e5526659
Kyohei Sakai <nico_f00tb@yahoo.co.jp>
2016-06-28 15:12:46 +0900

    no message

--- NewsReader/ViewController.swift
+++ NewsReader/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    //ニュース一覧データを格納する配列
+    var newsDataArray = NSArray()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //ニュース情報の取得先
+        let requestUrl = "http://appcre.net/rss.php"
+        //Webサーバに対してHTTP通信のリクエストを出してデータを取得
+        Alamofire.request(.GET, requestUrl).responseJSON { response in
+            switch response.result {
+            case .Success(let json):
+                //JSONデータをNSDictionaryに
+                let jsonDic = json as! NSDictionary
+                //辞書化したjsonDicからキー値"responseData"を取り出す
+                let responseData = jsonDic["responseData"] as! NSDictionary
+                //responseDataからキー値"results"を取り出す
+                self.newsDataArray = responseData["results"] as! NSArray
+                print("\(self.newsDataArray)")
+                
+            case .Failure(let error):
+                print("通信エラー：\(error)")
+            }
+        }
+        
+        //ニュース記事データをテーブルビューに表示
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
